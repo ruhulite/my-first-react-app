d55c22eafa495fff300f8320a2de5cffe0d9b51f
Md. Ruhul AMin Khan <corporate.ruhul@gmail.com>
2019-06-24 21:15:35 +0600

    file change

--- src/header.js
+++ src/header.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import HeaderStyle from './Header.css';
+import { Link } from "react-router-dom";
+import { linkList } from './pageLinks';
+
 
 class Header extends React.Component {
 	render() {
 		return(
 			<div className="site-header-wrap">
-				<div className="header-left"><h1>Logo</h1></div>
-				<div className="header-right">header right area.</div>
+				<div className="header-left"><h1><a href="/">Logo</a></h1></div>
+				<div className="header-right">
+					<nav>
+						<ul className="main-menu">
+							{linkList.map(({path, linkName}, index) =>
+							    <Link to={path} key = {index}> {linkName} </Link>
+							)}
+						</ul>
+					</nav>
+				</div>
 			</div>
 		);
 	}
---
+++ src/homeContent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Content extends React.Component {
+	render() {
+		return(
+			<div>
+				<section className="">
+					<h3>Content title...</h3>
+				</section>
+			</div>
+		);
+	}
+}
+
+export default Content;
