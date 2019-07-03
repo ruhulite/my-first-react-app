ade13505a2c9f4a700d88d3ff48d9fd2774c1ba5
Md. Ruhul AMin Khan <corporate.ruhul@gmail.com>
2019-07-03 20:13:20 +0600

    home content

--- src/home.js
+++ src/home.js
@@ -24,6 +24,25 @@ const personData = [
 					{age: 21, fname: 'Mathew', lname: 'Matt', department: 'IT', company: 'EchoTechSys'},
 				];
 
+const tabsData = [
+					{
+						title: 'First', 
+						tabData: 'Authoritatively empower future-proof vortals and interdependent materials. Continually enable scalable total linkage whereas open-source technologies. Phosfluorescently incubate compelling customer service rather than worldwide outsourcing. Distinctively synthesize equity invested scenarios vis-a-vis enterprise-wide web services. Assertively harness bleeding-edge total linkage after seamless partnerships. Continually customize unique total linkage vis-a-vis an expanded array of markets. Quickly e-enable cost effective synergy vis-a-vis fully tested best practices. Rapidiously target unique value before synergistic best practices. Progressively disseminate 24/7 synergy before interdependent web-readiness. Globally create economically sound portals through multidisciplinary total linkage. Quickly pursue high-quality quality vectors before impactful supply chains. Energistically administrate 24/7 e-commerce whereas value-added data. Globally recaptiualize vertical applications after viral sources. Holisticly envisioneer next-generation internal or "organic" sources and turnkey meta-services. Dramatically create premier e-markets via client-centered niches. Proactively target cooperative "outside the box" thinking after diverse platforms. Efficiently customize reliable systems and process-centric resources. Holisticly integrate web-enabled channels vis-a-vis out-of-the-box e-business. Interactively network superior manufactured products and robust ideas. Dramatically maximize sticky niches via one-to-one "outside the box" thinking. Authoritatively optimize worldwide catalysts for change through.',
+					},
+					{
+						title: 'Second', 
+						tabData: 'Compellingly maximize ubiquitous convergence vis-a-vis worldwide e-commerce. Professionally mesh standardized imperatives and multimedia based channels. Collaboratively visualize functional interfaces through 2.0 solutions. Professionally orchestrate interoperable results without clicks-and-mortar process improvements. Globally strategize holistic schemas vis-a-vis user friendly meta-services.',
+					},
+					{
+						title: 'Third', 
+						tabData: 'Dramatically evisculate progressive models before error-free resources. Distinctively benchmark client-centered growth strategies through orthogonal opportunities. Quickly iterate B2C customer service whereas turnkey sources. Monotonectally develop best-of-breed "outside the box" thinking through frictionless users. Authoritatively impact distinctive action items before ubiquitous outsourcing.',
+					},
+					{
+						title: 'Fourth', 
+						tabData: 'Conveniently embrace functional action items rather than premium leadership skills. Intrinsicly provide access to next-generation initiatives without high-quality value. Authoritatively synergize business architectures rather than timely process improvements. Conveniently expedite interactive e-markets for cross-unit applications. Intrinsicly cultivate proactive models after leading-edge results.',
+					},
+				];
+
 class HomeContent extends React.Component {
 	constructor (props) {
 		super(props);
@@ -36,6 +55,7 @@ class HomeContent extends React.Component {
 			age: '',
 			department: '',
 			company: '',
+			tabIndex: 0,
 		};
 		this.employeeInfoSubmitHandler = this.employeeInfoSubmitHandler.bind(this);
 	}
@@ -85,11 +105,16 @@ class HomeContent extends React.Component {
 
 	}
 
-	handleFieldValueChange = (event, fieldName) => this.setState({[fieldName]: event.target.value})
+	handleFieldValueChange = (event, fieldName) => this.setState({[fieldName]: event.target.value});
+
+	tabsClickHandler = (tabIndex) => {
+		this.setState({tabIndex});
+	}
 
 	render() {
-		const {isloggedIn, personInfo} = this.state;
+		const {isloggedIn, personInfo, tabIndex} = this.state;
 		const {age = '', department = '', company = ''} = personInfo[this.state.id] || {};
+		const {tabData} = tabsData[tabIndex];
 		return(
 			<div>
 				<section className="home-main-content">					
@@ -142,6 +167,19 @@ class HomeContent extends React.Component {
 								<input type="submit" value="Submit" />
 							</div>
 						</form>
+					</div>
+					<h3 className="re-employee-information">About the Employee</h3>
+					<div className="employee-information-form">
+						<div className="employee-tabs-wrap">
+							<ul className="employee-tabs">
+								{tabsData.map(({title}, index) =>
+								<li className={tabIndex === index ? 'active' : ''} key={index} onClick={() => this.tabsClickHandler(index)}><span className="tab-title">{title}</span></li>
+								)}
+							</ul>
+						</div>
+						<div className="employee-tabs-body">
+							{tabData}
+						</div>
 					</div>
 				</section>
 			</div>
