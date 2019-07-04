c23fef51b79e02c89706342bc16f116c7401d6a8
Md. Ruhul AMin Khan <corporate.ruhul@gmail.com>
2019-07-04 21:51:25 +0600

    home content

--- src/header.js
+++ src/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeaderStyle from './Header.css';
+import HeaderStyle from './assets/css/Header.css';
 import { Link } from "react-router-dom";
 import { linkList } from './pageLinks';
 
@@ -12,14 +12,37 @@ class Header extends React.Component {
 				<div className="header-right">
 					<nav>
 						<ul className="main-menu">
-							{linkList.map(({path, linkName}, index) =>
+							{linkList.map(({path, linkName, submenu1}, index) =>
 							    <li>
 							    	<Link to={path} key = {index}> {linkName} </Link>
-							    	<ul className="submenu">
-										<li>
-											{/*<Link to={path} key = {index}> {linkName} </Link>*/}
-										</li>
-									</ul>
+							    	{submenu1 ? 
+								    	<ul className="submenu">											
+											{submenu1.map(({path, title, submenu2}, index) =>
+												<li>
+													<Link to={path} key = {index}> {title} </Link>
+													{submenu2 ? 
+												    	<ul className="submenu">											
+															{submenu2.map(({path, title, submenu3}, index) =>
+																<li>
+																	<Link to={path} key = {index}> {title} </Link>
+																	{submenu3 ? 
+																    	<ul className="submenu">											
+																			{submenu3.map(({path, title, submenu3}, index) =>
+																				<li><Link to={path} key = {index}> {title} </Link></li>
+																			)}											
+																		</ul>
+																	: null
+																	}
+																</li>
+															)}											
+														</ul>
+													: null
+													}
+												</li>
+											)}											
+										</ul>
+									: null
+									}
 							    </li>
 							)}
 						</ul>
--- src/pageLinks.js
+++ src/pageLinks.js
@@ -5,12 +5,24 @@ import ServiceContent from './service';
 export const linkList = [
 					{exact: true, path: '/', linkName: 'home', component: HomeContent, 
 						submenu1: [
-							'Home Submenu 1',
-							'Home Submenu 2',
-							'Home Submenu 3',
-							'Home Submenu 4',
-							'Home Submenu 5',
-						]
+							{path: 'home-submenu-1', title: 'Home Submenu 1'},
+							{path: 'home-submenu-2', title: 'Home Submenu 2',
+								submenu2: [
+									{path: 'home-submenu-1-1', title: 'Home Submenu 1-1'},
+									{path: 'home-submenu-1-2', title: 'Home Submenu 1-2',
+										submenu3: [
+											{path: 'home-submenu-1-1-1', title: 'Home Submenu 1-1-1'},
+											{path: 'home-submenu-1-1-2', title: 'Home Submenu 1-1-2'},
+											{path: 'home-submenu-1-1-3', title: 'Home Submenu 1-1-3'},
+										],
+								},
+									{path: 'home-submenu-1-3', title: 'Home Submenu 1-3'},
+								],
+							},
+							{path: 'home-submenu-3', title: 'Home Submenu 3'},
+							{path: 'home-submenu-4', title: 'Home Submenu 4'},
+							{path: 'home-submenu-5', title: 'Home Submenu 5'},
+						],
 					}, 
 					{exact: false, path: '/about', linkName: 'about', component: AboutContent}, 
 					{exact: false, path: '/service', linkName: 'service', component: ServiceContent},
