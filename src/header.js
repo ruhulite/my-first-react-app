398ef8688922af97873359c8d36e75a18c18be3c
Md. Ruhul AMin Khan <corporate.ruhul@gmail.com>
2019-07-10 22:10:11 +0600

    home content

--- src/header.js
+++ src/header.js
@@ -2,44 +2,40 @@ import React from 'react';
 import HeaderStyle from './assets/css/Header.css';
 import { Link } from "react-router-dom";
 import { linkList } from './pageLinks';
+import mainMenu from './linkLists';
 
 
 class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			menuItem: mainMenu.menuItem,
+		};
+	}
+
+	
+
 	render() {
+		const {menuItem} = this.state;
+
 		return(
 			<div className="site-header-wrap">
 				<div className="header-left"><h1><a href="/">Logo</a></h1></div>
 				<div className="header-right">
 					<nav>
+						
+
 						<ul className="main-menu">
-							{linkList.map(({path, linkName, submenu1}, index) =>
-							    <li>
-							    	<Link to={path} key = {index}> {linkName} </Link>
-							    	{submenu1 ? 
-								    	<ul className="submenu">											
-											{submenu1.map(({path, title, submenu2}, index) =>
-												<li>
-													<Link to={path} key = {index}> {title} </Link>
-													{submenu2 ? 
-												    	<ul className="submenu">											
-															{submenu2.map(({path, title, submenu3}, index) =>
-																<li>
-																	<Link to={path} key = {index}> {title} </Link>
-																	{submenu3 ? 
-																    	<ul className="submenu">											
-																			{submenu3.map(({path, title, submenu3}, index) =>
-																				<li><Link to={path} key = {index}> {title} </Link></li>
-																			)}											
-																		</ul>
-																	: null
-																	}
-																</li>
-															)}											
-														</ul>
-													: null
-													}
-												</li>
-											)}											
+							{menuItem.map(({path, title, submenu}, index) =>
+							    <li key={title}>
+							    	<Link to={path} key={title}> {title} </Link>
+							    	{submenu && submenu.length > 0 ? 
+								    	<ul className="submenu">
+								    		<li key={title}>
+												{submenu.map(({path, title}, index) =>
+													<Link to={path} key={index}> {title} </Link>
+												)}
+											</li>
 										</ul>
 									: null
 									}
